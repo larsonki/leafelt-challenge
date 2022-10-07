d37ed61c650fb7f57def9933d8a3e52624e4e4ac
larsonki <kirsten.ifill.larson@gmail.com>
2022-10-06 22:17:56 -0500

    updated README and commented code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# leafelt-challenge
+# Interactive Map - Earthquake and Tectonic Plate Visualization
+
+### Using the Leaflet JavaScript Library and information from the United States Geological Survey (USGS), all earthquakes recorded from the last month were plotted onto a world map.
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,4 @@
+// Create the map, centering on the contiguous US
 var myMap = L.map("map", {
   center: [39.8283, -98.5795],
   zoom: 5
@@ -19,14 +20,11 @@ function getColor(d) {
 // Add a legend to the map for the depth colorscale.
 // Code adapted from https://leafletjs.com/examples/choropleth/
 var legend = L.control({position: 'bottomright'});
-
 legend.onAdd = function (map) {
-
     var div = L.DomUtil.create('div', 'info legend'),
         depths = [-10, 10, 30, 50, 70, 90],
         labels = [];
-
-    // loop through our density intervals and generate a label with a colored square for each interval
+    // loop through the depth intervals and generate a label with a colored square for each interval
     for (var i = 0; i < depths.length; i++) {
         div.innerHTML +=
             '<i style="background:' + getColor(depths[i] + 1) + '"></i> ' +
@@ -35,30 +33,28 @@ legend.onAdd = function (map) {
 
     return div;
 };
-
 legend.addTo(myMap);
 
-// Adding the tile layer
+// Add the tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
+// Create a variable for the USGS URL
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
+// Read the URL and create a function to loop through each earthquake entry
 d3.json(url).then(function (response) {
 
   // console.log(response.features[0].geometry);
-
-  // i < response.features.length
-
-  for (var i = 0; i < 50; i++) {
+  for (var i = 0; i < response.features.length; i++) {
     var location = response.features[i].geometry;
     var magnitude = response.features[i].properties.mag;
     var place = response.features[i].properties.place
     var time = response.features[i].properties.time
 
     // console.log(location);
-
+    // Add circles for each earthquake, using the depth to determine the color and the magnitude to determine radius
     if (location) {
       L.circleMarker([location.coordinates[1], location.coordinates[0]], {
         fillOpacity: 1.0,
@@ -72,17 +68,18 @@ d3.json(url).then(function (response) {
   };
 });
 
+// Create a style for the tectonic plates portion of the map
 var mapStyle = {
   color: "orange",
   fillOpacity: 0
 };
 
+// Create a variable for the tectonic plates URL
 var platesUrl = 'https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json'
 
+// Read through the URL and add the geojson info to the map
 d3.json(platesUrl).then(function (response) {
   L.geoJSON(response, {
     style: mapStyle
   }).addTo(myMap);
 });
-
-
